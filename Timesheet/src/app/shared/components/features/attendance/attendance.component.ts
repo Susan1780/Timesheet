01f40112a8368bb34b8a71e9af416bc6dd631ab1
Aparajitha <susanisrael2000@gmail.com>
2023-02-03 11:29:33 +0530

    code clean up for attendance component

--- Timesheet/src/app/shared/components/features/attendance/attendance.component.ts
+++ Timesheet/src/app/shared/components/features/attendance/attendance.component.ts
@@ -16,8 +16,6 @@ export class AttendanceComponent implements OnInit {
   showTimeOff: boolean = true;
   showAbsence: boolean = false;
   isBtnShown: boolean = false;
-  // bool: boolean = true;
-  // bool1: boolean = false;
   balanceArray: any = ['Earned Leave', 'Special Leave'];
   holidayArray: any = [
     'Holi (Compulsory) (1 Day)',
@@ -49,53 +47,19 @@ export class AttendanceComponent implements OnInit {
       totaldays: '0',
       caldate2: this.currentDate,
     });
-
-    // if(this.form.leavetype === 'Half-a-day'){
-    //   this.form.setValue({
-    //     totaldays: '0.5'
-    //   });
-    // }
-
-    // if(this.bool1){
-    //   this.form.patchValue({
-    //     totaldays: '0.5'
-    //   });
-    // }
   }
 
-  myDateFilter = (d: Date): boolean => {
+  dateFilter = (d: Date): boolean => {
     const day = d.getDay();
     return day !== 0 && day !== 6;
   };
 
-  dateChanged1(event: any) {
-    // this.bool = false;
-    // this.bool1 = false;
-    this.date1 = event.target.value;
-  }
-
-  dateChanged2(event: any) {
-    // this.bool = false;
-    // this.bool1 = false;
-    this.date2 = event.target.value;
+  addEvent(event: any){
+    console.log(event);
+    this.calculateDays();
   }
 
   calculateDays() {
-    // debugger;
-    // if (this.bool === true) {
-    //   const startDate = new Date(this.selectedDate);
-    //   const endDate = new Date(this.currentDate);
-    //   const timeInterval = endDate.getTime() - startDate.getTime();
-    //   this.days = (timeInterval / (1000 * 3600 * 24)).toFixed();
-    // } else if (this.bool === false) {
-    //   const startingDate = this.bool1 ? new Date(this.date1) : new Date(this.selectedDate);
-    //   const endingDate = this.bool1 ? new Date(this.currentDate) : new Date(this.date2);
-    //   const timeBtwn = endingDate.getTime() - startingDate.getTime();
-    //   this.days = (timeBtwn / (1000 * 3600 * 24)).toFixed();
-    // } else {
-    //   return;
-    // }
-
       const startDate = this.form.controls['caldate1'].value;
       const endDate = this.form.controls['caldate2'].value;
       const timeInterval = endDate.getTime() - startDate.getTime();
@@ -104,12 +68,6 @@ export class AttendanceComponent implements OnInit {
     this.form.patchValue({
       totaldays: `${this.days} Days`,
     });
-  }
-  inputChanged(event:any){
-    this.startDate = event.target.value;
-  }
-  inputChanged2(event:any){
-    this.endDate = event.target.value;
   }
 
   showDialog() {
@@ -129,7 +87,7 @@ export class AttendanceComponent implements OnInit {
     alert('Submitted successfully!');
     console.log(this.form.value);
     console.log(this.form.controls['caldate1'].value)
-    // this.form.reset();
+    this.form.reset();
     this.showTimeOff = true;
     this.showAbsence = false;
     this.isBtnShown = false;
