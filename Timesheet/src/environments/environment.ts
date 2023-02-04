921a79c7c40c30d4ea87a455ae98261c760591a3
Aparajitha <susanisrael2000@gmail.com>
2023-02-04 12:20:10 +0530

    Merge conflict resolved

--- Timesheet/src/environments/environment.ts
+++ Timesheet/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyC0JlMSiH9S2rN7Ej-ajX9GF8ovQRVUSZ8",
+    authDomain: "timesheet-8af42.firebaseapp.com",
+    projectId: "timesheet-8af42",
+    storageBucket: "timesheet-8af42.appspot.com",
+    messagingSenderId: "1064125367847",
+    appId: "1:1064125367847:web:10a3f5e84da22cd1a8f002",
+    measurementId: "G-22WR5C1G52"
+  }
 };
 
 /*
